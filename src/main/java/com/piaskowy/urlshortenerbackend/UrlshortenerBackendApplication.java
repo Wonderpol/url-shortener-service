88daad4c44731ce9b9b3a3016b23c773db864a62
Jan Piaskowy <piaskowyjasiek@gmail.com>
2023-01-03 16:39:50 +0100

    app: initial commit

---
+++ src/main/java/com/piaskowy/urlshortenerbackend/UrlshortenerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.piaskowy.urlshortenerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortenerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlshortenerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piaskowy/urlshortenerbackend/UrlshortenerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piaskowy.urlshortenerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
