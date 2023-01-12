70ab1d792d9c5cb2c3cb533880397908f4011b9d
Jan Piaskowy <piaskowyjasiek@gmail.com>
2023-01-12 21:55:36 +0100

    tests: covered UserRegisterationService with tests

---
+++ src/test/java/com/piaskowy/urlshortenerbackend/auth/user/service/UserRegistrationServiceTest.java
@@ -0,0 +1,72 @@
+package com.piaskowy.urlshortenerbackend.auth.user.service;
+
+import com.piaskowy.urlshortenerbackend.auth.user.exception.UserAlreadyExistsException;
+import com.piaskowy.urlshortenerbackend.auth.user.model.entity.User;
+import com.piaskowy.urlshortenerbackend.auth.user.model.request.RegisterRequest;
+import com.piaskowy.urlshortenerbackend.auth.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UserRegistrationServiceTest {
+
+    @InjectMocks
+    UserRegistrationService userRegistrationService;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    void test_signUpNewUser_shouldThrow_UserAlreadyExistsException() {
+        //given
+        RegisterRequest registerRequest = RegisterRequest.builder()
+                .email("test@test.com")
+                .build();
+        given(userRepository.findByEmail(anyString())).willReturn(Optional.of(new User()));
+        //when
+        //then
+        assertThatThrownBy(() -> userRegistrationService.signUpNewUser(registerRequest))
+                .isInstanceOf(UserAlreadyExistsException.class)
+                .hasMessageContaining("User with email: " + registerRequest.getEmail() + " already exists");
+    }
+
+    @Test
+    void test_signUpNewUser() {
+        //given
+
+        RegisterRequest registerRequest = RegisterRequest.builder()
+                .name("John")
+                .lastName("Kowalski")
+                .email("test@test.com")
+                .password("password")
+                .build();
+
+        given(userRepository.findByEmail(anyString())).willReturn(Optional.empty());
+        given(passwordEncoder.encode("password")).willReturn("encoded_password");
+        //when
+        userRegistrationService.signUpNewUser(registerRequest);
+        //then
+        final ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).saveAndFlush(userArgumentCaptor.capture());
+
+        final User capturedValue = userArgumentCaptor.getValue();
+
+        assertThat(registerRequest.getEmail()).isEqualTo(capturedValue.getEmail());
+        assertThat("encoded_password").isEqualTo(capturedValue.getPassword());
+    }
+
+}
