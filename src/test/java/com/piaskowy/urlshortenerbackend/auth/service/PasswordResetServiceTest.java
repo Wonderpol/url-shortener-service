8ac781ac22c24871e003e08a351409f3a8f7594c
Jan Piaskowy <piaskowyjasiek@gmail.com>
2023-03-15 18:39:42 +0100

    test: test method sendPasswordResetEmail not throws

--- src/test/java/com/piaskowy/urlshortenerbackend/auth/service/PasswordResetServiceTest.java
+++ src/test/java/com/piaskowy/urlshortenerbackend/auth/service/PasswordResetServiceTest.java
@@ -2,14 +2,22 @@ package com.piaskowy.urlshortenerbackend.auth.service;
 
 import com.piaskowy.urlshortenerbackend.config.EnvironmentVariables;
 import com.piaskowy.urlshortenerbackend.email.EmailService;
+import com.piaskowy.urlshortenerbackend.email.model.Email;
+import com.piaskowy.urlshortenerbackend.user.model.entity.User;
+import jakarta.mail.MessagingException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class PasswordResetServiceTest {
@@ -37,6 +45,36 @@ class PasswordResetServiceTest {
 
         //then
         assertEquals(expectedLink, generatedLink);
+    }
+
+    @Test
+    void test_sendPasswordResetEmail() throws MessagingException {
+        //given
+        String token = "12345678";
+        User user = User.builder()
+                .id(1L)
+                .email("example@example.com")
+                .password("test")
+                .name("Test")
+                .lastName("Test")
+                .build();
+
+        Map<String, Object> expectedProperties = new HashMap<>();
+        expectedProperties.put("name", user.getName());
+        expectedProperties.put("link", passwordResetService.createResetPasswordLink(token));
+
+        given(environmentVariables.getResetPasswordTemplateName()).willReturn("reset-password.html");
+
+        //when
+        passwordResetService.sendPasswordResetEmail(token, user);
+        //then
+        ArgumentCaptor<Email> emailArgumentCaptor = ArgumentCaptor.forClass(Email.class);
+        verify(emailService).sendHtmlEmail(emailArgumentCaptor.capture());
+        Email sentEmail = emailArgumentCaptor.getValue();
+        assertEquals(user.getEmail(), sentEmail.to());
+        assertEquals("noreply@test.com", sentEmail.from());
+        assertEquals(environmentVariables.getResetPasswordTemplateName(), sentEmail.template());
+        assertEquals(expectedProperties, sentEmail.properties());
     }
 
 }
